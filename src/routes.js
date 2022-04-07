cfbaf6ced81196c9031029b9d204d80f19801f40
Sania Bening Nareswara <72061252+sanianrs@users.noreply.github.com>
2022-04-07 14:27:36 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+const {
+  addBooks, getAllBooks, getBooksById, editBook, deleteBooks,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooks,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooks,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBooksById,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBook,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBooks,
+  },
+];
+
+module.exports = routes;
